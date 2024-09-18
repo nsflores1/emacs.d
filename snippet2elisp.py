590d47d99aaef3a8b78ffadeb33ce6f091ee412d
Nathaniel Flores <71289838+nsflores1@users.noreply.github.com>
2024-09-18 09:26:46 -0400

    Add future room for growth, begin work

--- snippet2elisp.py
+++ snippet2elisp.py
@@ -4,13 +4,14 @@ import re, os, argparse
 parse = argparse.ArgumentParser("snippet2elisp",
                                 description="A small tool for converting Markdown code snippets to Emacs Lisp.",
                                 epilog="For more information, please read the enclosed README.md that came with this configuration.")
-parse.add_argument('-f', '--file', help="The file to convert.")
-parse.add_argument('-q', '--quiet', help="Disable messages.")
+parse.add_argument('file', type=str, help="The file to convert.")
+parse.add_argument('-q', action='store_true', help="Disable messages.")
 args = parse.parse_args()
 
-with open('elisp.md', 'r', encoding="utf-8") as f:
+with open(args.file, 'r', encoding="utf-8") as f:
  regex_text = f.read()
  result = re.findall(r'```(.*?)```', regex_text, re.S)
- print(result)
+ for i in result:
+  print(i)
 
 print(args)
